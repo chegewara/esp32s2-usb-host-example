74d8cae11f55ebd888cf924d5d00cd95278babfa
chegewara <imperiaonline4@gmail.com>
2021-03-27 16:54:44 +0100

    Refactor to use callbacks

--- main/ctrl_pipe.c
+++ main/ctrl_pipe.c
@@ -14,16 +14,11 @@
 #include "hcd.h"
 #include "ctrl_pipe.h"
 
-#define EVENT_QUEUE_LEN         10
-#define NUM_XFER_REQS           4
-#define XFER_DATA_MAX_LEN       64     //Just assume that will only IN/OUT 64 bytes for now
-#define PORT_NUM                1
+hcd_pipe_handle_t ctrl_pipe_hdl;
+hcd_xfer_req_handle_t ctrl_req_hdls[NUM_XFER_REQS];
+uint8_t *ctrl_data_buffers[NUM_XFER_REQS];
+usb_irp_t *ctrl_irps[NUM_XFER_REQS];
 
-
-static hcd_pipe_handle_t ctrl_pipe_hdl;
-static hcd_xfer_req_handle_t req_hdls[NUM_XFER_REQS];
-static uint8_t *data_buffers[NUM_XFER_REQS];
-static usb_irp_t *irps[NUM_XFER_REQS];
 static QueueHandle_t ctrl_pipe_evt_queue;
 static ctrl_pipe_cb_t ctrl_pipe_cb;
 uint8_t bMaxPacketSize0;
@@ -61,9 +56,9 @@ void free_pipe_and_xfer_reqs_ctrl(hcd_pipe_handle_t pipe_hdl)
     ESP_LOGD("", "Freeing transfer requets\n");
     //Free transfer requests (and their associated objects such as IRPs and data buffers)
     for (int i = 0; i < NUM_XFER_REQS; i++) {
-        heap_caps_free(irps[i]);
-        heap_caps_free(data_buffers[i]);
-        hcd_xfer_req_free(req_hdls[i]);
+        heap_caps_free(ctrl_irps[i]);
+        heap_caps_free(ctrl_data_buffers[i]);
+        hcd_xfer_req_free(ctrl_req_hdls[i]);
     }
     ESP_LOGD("", "Freeing pipe\n");
     //Delete the pipe
@@ -97,16 +92,16 @@ void alloc_pipe_and_xfer_reqs_ctrl(hcd_port_handle_t port_hdl, hcd_pipe_handle_t
     // printf("Creating transfer requests\n");
     for (int i = 0; i < NUM_XFER_REQS; i++) {
         //Allocate transfer request object
-        req_hdls[i] = hcd_xfer_req_alloc();
-        if(NULL == req_hdls[i]) ESP_LOGE("", "err 4");
+        ctrl_req_hdls[i] = hcd_xfer_req_alloc();
+        if(NULL == ctrl_req_hdls[i]) ESP_LOGE("", "err 4");
         //Allocate data buffers
-        data_buffers[i] = heap_caps_calloc(1, sizeof(usb_ctrl_req_t) + XFER_DATA_MAX_LEN, MALLOC_CAP_DMA);
-        if(NULL == data_buffers[i]) ESP_LOGE("", "err 5");
+        ctrl_data_buffers[i] = heap_caps_calloc(1, sizeof(usb_ctrl_req_t) + XFER_DATA_MAX_LEN, MALLOC_CAP_DMA);
+        if(NULL == ctrl_data_buffers[i]) ESP_LOGE("", "err 5");
         //Allocate IRP object
-        irps[i] = heap_caps_malloc(sizeof(usb_irp_t), MALLOC_CAP_DEFAULT);
-        if(NULL == irps[i]) ESP_LOGE("", "err 6");
+        ctrl_irps[i] = heap_caps_malloc(sizeof(usb_irp_t), MALLOC_CAP_DEFAULT);
+        if(NULL == ctrl_irps[i]) ESP_LOGE("", "err 6");
         //Set the transfer request's target
-        hcd_xfer_req_set_target(req_hdls[i], ctrl_pipe_hdl, irps[i], (void*)i);
+        hcd_xfer_req_set_target(ctrl_req_hdls[i], ctrl_pipe_hdl, ctrl_irps[i], (void*)i);
     }
     *handle = ctrl_pipe_hdl;
 }
@@ -120,99 +115,200 @@ void ctrl_pipe_event_task(void* p)
     while(1){
         xQueueReceive(ctrl_pipe_evt_queue, &msg, portMAX_DELAY);
         hcd_xfer_req_handle_t req_hdl = hcd_xfer_req_dequeue(msg.pipe_hdl);
+        hcd_pipe_handle_t pipe_hdl;
+        usb_irp_t *irp;
+        void *context;
         if(req_hdl == NULL) continue;
+
+        hcd_xfer_req_get_target(req_hdl, &pipe_hdl, &irp, &context);
+
+        usb_ctrl_req_t* ctrl = (usb_ctrl_req_t*)irp->data_buffer;
+
+        switch (msg.pipe_event)
+        {
+            case HCD_PIPE_EVENT_NONE:
+                break;
+
+            case HCD_PIPE_EVENT_XFER_REQ_DONE:
+                switch (ctrl->bRequest)
+                {
+                    case USB_B_REQUEST_GET_STATUS:
+                        break;
+
+                    case USB_B_REQUEST_CLEAR_FEATURE:
+                        break;
+
+                    case USB_B_REQUEST_SET_FEATURE:
+                        break;
+
+                    case USB_B_REQUEST_SET_ADDRESS:
+                        usbh_set_address_cb(ctrl->wValue, context);
+                        break;
+
+                    case USB_B_REQUEST_GET_DESCRIPTOR:
+                        switch ((ctrl->wValue >> 8) & 0xff)
+                        {
+                            case USB_W_VALUE_DT_DEVICE:
+                                usbh_get_device_desc_cb(irp->data_buffer + sizeof(usb_ctrl_req_t), irp->actual_num_bytes, context);
+                                break;
+                            
+                            case USB_W_VALUE_DT_CONFIG:
+                                usbh_get_config_desc_cb(irp->data_buffer + sizeof(usb_ctrl_req_t), irp->actual_num_bytes, context);
+                                break;
+                            
+                            case USB_W_VALUE_DT_STRING:
+                                usbh_get_string_cb((irp->data_buffer + sizeof(usb_ctrl_req_t)), irp->actual_num_bytes, context);
+                                break;
+
+                            case USB_W_VALUE_DT_INTERFACE:
+                                usbh_get_interface_desc_cb(irp->data_buffer + sizeof(usb_ctrl_req_t), irp->actual_num_bytes, context);
+                                break;
+                            
+                            case USB_W_VALUE_DT_ENDPOINT:
+                                usbh_get_endpoint_desc_cb(irp->data_buffer + sizeof(usb_ctrl_req_t), irp->actual_num_bytes, context);
+                                break;
+                            
+                            case USB_W_VALUE_DT_DEVICE_QUALIFIER:
+                                break;
+                            
+                            case USB_W_VALUE_DT_OTHER_SPEED_CONFIG:
+                                break;
+                            
+                            case USB_W_VALUE_DT_INTERFACE_POWER:
+                                usbh_get_power_desc_cb(irp->data_buffer + sizeof(usb_ctrl_req_t), irp->actual_num_bytes, context);
+                                break;
+                            
+                            default:
+                                break;
+                        }
+                        break;
+
+                    case USB_B_REQUEST_GET_CONFIGURATION:
+                        usbh_get_configuration_cb(*(uint8_t*)(irp->data_buffer + sizeof(usb_ctrl_req_t)), context);
+                        break;
+
+                    case USB_B_REQUEST_SET_CONFIGURATION:
+                        usbh_set_config_desc_cb(ctrl->wValue, context);
+                        break;
+
+                    case USB_B_REQUEST_SET_INTERFACE:
+                        break;
+
+                    case USB_B_REQUEST_SYNCH_FRAME:
+                    ESP_LOGI("", "SYNC");
+                        break;
+
+                    default:
+                        usbh_ctrl_pipe_class_specific_cb(msg, irp);
+                        break;
+                }
+            break;
+
+        case HCD_PIPE_EVENT_ERROR_XFER:
+            usbh_ctrl_pipe_error_cb(ctrl);
+            ESP_LOGW("", "XFER error: %d", irp->status);
+            hcd_pipe_command(msg.pipe_hdl, HCD_PIPE_CMD_RESET);
+            break;
+        
+        case HCD_PIPE_EVENT_ERROR_STALL:
+            usbh_ctrl_pipe_stalled_cb(ctrl);
+            ESP_LOGW("", "Device stalled: %s pipe, state: %d", msg.pipe_hdl == ctrl_pipe_hdl?"CTRL":"BULK", hcd_pipe_get_state(msg.pipe_hdl));
+            ESP_LOG_BUFFER_HEX_LEVEL("Ctrl data", ctrl, sizeof(usb_ctrl_req_t), ESP_LOG_INFO);
+            hcd_pipe_command(msg.pipe_hdl, HCD_PIPE_CMD_RESET);
+            break;
+
+            default:
+                break;
+        }
         if (ctrl_pipe_cb != NULL)
         {
-            ctrl_pipe_cb(msg, req_hdl);
+            ctrl_pipe_cb(msg, irp, context);
         }
     }
 }
 
 void xfer_get_device_desc()
 {
-    USB_CTRL_REQ_INIT_GET_DEVC_DESC((usb_ctrl_req_t *) data_buffers[0]);
+    USB_CTRL_REQ_INIT_GET_DEVC_DESC((usb_ctrl_req_t *) ctrl_data_buffers[0]);
 
-    irps[0]->num_bytes = bMaxPacketSize0;    //1 worst case MPS
-    irps[0]->data_buffer = data_buffers[0];
-    irps[0]->num_iso_packets = 0;
+    ctrl_irps[0]->num_bytes = bMaxPacketSize0;    //1 worst case MPS
+    ctrl_irps[0]->data_buffer = ctrl_data_buffers[0];
+    ctrl_irps[0]->num_iso_packets = 0;
 
 
     //Enqueue those transfer requests
-    if(ESP_OK == hcd_xfer_req_enqueue(req_hdls[0])) {
+    if(ESP_OK == hcd_xfer_req_enqueue(ctrl_req_hdls[0])) {
         ESP_LOGD("", "Get device desc");
     }
 }
 
 void xfer_set_address(uint8_t addr)
 {
-    USB_CTRL_REQ_INIT_SET_ADDR((usb_ctrl_req_t *) data_buffers[0], addr);
+    USB_CTRL_REQ_INIT_SET_ADDR((usb_ctrl_req_t *) ctrl_data_buffers[0], addr);
 
-    irps[0]->num_bytes = bMaxPacketSize0;    //1 worst case MPS
-    irps[0]->data_buffer = data_buffers[0];
-    irps[0]->num_iso_packets = 0;
-    irps[0]->num_bytes = 0;
+    ctrl_irps[0]->num_bytes = bMaxPacketSize0;    //1 worst case MPS
+    ctrl_irps[0]->data_buffer = ctrl_data_buffers[0];
+    ctrl_irps[0]->num_iso_packets = 0;
+    ctrl_irps[0]->num_bytes = 0;
 
 
     //Enqueue those transfer requests
-    if(ESP_OK == hcd_xfer_req_enqueue(req_hdls[0])) {
+    if(ESP_OK == hcd_xfer_req_enqueue(ctrl_req_hdls[0])) {
         ESP_LOGD("", "Set address");
     }
 }
 
 void xfer_get_current_config()
 {
-    USB_CTRL_REQ_INIT_GET_CONFIG((usb_ctrl_req_t *) data_buffers[0]);
-    irps[0]->num_bytes = bMaxPacketSize0;    //1 worst case MPS
-    irps[0]->data_buffer = data_buffers[0];
-    irps[0]->num_iso_packets = 0;
-    irps[0]->num_bytes = 0;
-
+    USB_CTRL_REQ_INIT_GET_CONFIG((usb_ctrl_req_t *) ctrl_data_buffers[0]);
+    ctrl_irps[0]->num_bytes = bMaxPacketSize0;    //1 worst case MPS
+    ctrl_irps[0]->data_buffer = ctrl_data_buffers[0];
+    ctrl_irps[0]->num_iso_packets = 0;
 
     //Enqueue those transfer requests
-    if(ESP_OK == hcd_xfer_req_enqueue(req_hdls[0])) {
+    if(ESP_OK == hcd_xfer_req_enqueue(ctrl_req_hdls[0])) {
         ESP_LOGD("", "Get current config");
     }
 }
 
 void xfer_set_configuration(uint8_t num)
 {
-    USB_CTRL_REQ_INIT_SET_CONFIG((usb_ctrl_req_t *) data_buffers[2], num);
-    irps[2]->num_bytes = bMaxPacketSize0;    //1 worst case MPS
-    irps[2]->data_buffer = data_buffers[2];
-    irps[2]->num_iso_packets = 0;
-    irps[2]->num_bytes = 0;
-
+    USB_CTRL_REQ_INIT_SET_CONFIG((usb_ctrl_req_t *) ctrl_data_buffers[2], num);
+    ctrl_irps[2]->num_bytes = bMaxPacketSize0;    //1 worst case MPS
+    ctrl_irps[2]->data_buffer = ctrl_data_buffers[2];
+    ctrl_irps[2]->num_iso_packets = 0;
+    ctrl_irps[2]->num_bytes = 0;
 
     //Enqueue those transfer requests
-    if(ESP_OK == hcd_xfer_req_enqueue(req_hdls[2])) {
+    if(ESP_OK == hcd_xfer_req_enqueue(ctrl_req_hdls[2])) {
         ESP_LOGD("", "Get current config");
     }
 }
 
 void xfer_get_desc()
 {
-    USB_CTRL_REQ_INIT_GET_CFG_DESC((usb_ctrl_req_t *) data_buffers[0], 1, XFER_DATA_MAX_LEN);
+    USB_CTRL_REQ_INIT_GET_CFG_DESC((usb_ctrl_req_t *) ctrl_data_buffers[0], 1, XFER_DATA_MAX_LEN);
     //important!! if is shorter than buffer and descriptor is longer than num_bytes, then it will stuck here
     // so its best if both values are equal
-    irps[0]->num_bytes = XFER_DATA_MAX_LEN;
-    irps[0]->data_buffer = data_buffers[0];
-    irps[0]->num_iso_packets = 0;
+    ctrl_irps[0]->num_bytes = XFER_DATA_MAX_LEN;
+    ctrl_irps[0]->data_buffer = ctrl_data_buffers[0];
+    ctrl_irps[0]->num_iso_packets = 0;
 
     //Enqueue those transfer requests
-    if(ESP_OK == hcd_xfer_req_enqueue(req_hdls[0])) {
+    if(ESP_OK == hcd_xfer_req_enqueue(ctrl_req_hdls[0])) {
         ESP_LOGD("", "Get config desc");
     }
 }
 
 void xfer_get_string(uint8_t num)
 {
-    USB_CTRL_REQ_INIT_GET_STRING((usb_ctrl_req_t *) data_buffers[num], 0, num, XFER_DATA_MAX_LEN);
-    irps[num]->num_bytes = XFER_DATA_MAX_LEN;
-    irps[num]->data_buffer = data_buffers[num];
-    irps[num]->num_iso_packets = 0;
+    USB_CTRL_REQ_INIT_GET_STRING((usb_ctrl_req_t *) ctrl_data_buffers[num], 0, num, XFER_DATA_MAX_LEN);
+    ctrl_irps[num]->num_bytes = XFER_DATA_MAX_LEN;
+    ctrl_irps[num]->data_buffer = ctrl_data_buffers[num];
+    ctrl_irps[num]->num_iso_packets = 0;
 
     //Enqueue those transfer requests
-    if(ESP_OK == hcd_xfer_req_enqueue(req_hdls[num])) {
+    if(ESP_OK == hcd_xfer_req_enqueue(ctrl_req_hdls[num])) {
         ESP_LOGD("", "Get string: %d", num);
     }
 }
--- main/main.c
+++ main/main.c
@@ -1,5 +1,4 @@
-
-/* Hello World Example
+/* USB host example
 
    This example code is in the Public Domain (or CC0 licensed, at your option.)
 
@@ -22,6 +21,8 @@
 #include "ctrl_pipe.h"
 #include "usb_host_port.h"
 
+// #define USE_ALTERNATIVE_CALLBACKS
+
 uint8_t device_state = 0;
 uint8_t conf_num;
 
@@ -32,13 +33,80 @@ uint8_t bMaxPacketSize0 = 64;
 uint8_t conf_num = 0;
 void parse_cfg_descriptor(uint8_t* data_buffer, usb_transfer_status_t status, uint8_t len, uint8_t* conf_num);
 
-static void ctrl_pipe_cb(pipe_event_msg_t msg, hcd_xfer_req_handle_t req_hdl)
+static void utf16_to_utf8(char* in, char* out, uint8_t len)
 {
-    hcd_pipe_handle_t pipe_hdl;
-    usb_irp_t *irp;
-    void *context;
-    hcd_xfer_req_get_target(req_hdl, &pipe_hdl, &irp, &context);
-    // ESP_LOGD("", "\t-> Pipe [%d] event: %d\n", (uint8_t)context, msg.pipe_event);
+    for (size_t i = 0; i < len; i++)
+    {
+        out[i/2] = in[i];
+        i++;
+    }
+}
+
+
+/*------------------------------- USBH EP0 CTRL PIPE CALLBACKS -------------------------------*/
+#ifndef USE_ALTERNATIVE_CALLBACKS
+void usbh_get_device_desc_cb(uint8_t* data_buffer, size_t num_bytes, void* context)
+{
+    ESP_LOG_BUFFER_HEX_LEVEL("DEVICE descriptor", data_buffer, num_bytes, ESP_LOG_INFO);
+    parse_cfg_descriptor(data_buffer, 0, num_bytes, &conf_num);
+    xfer_set_address(DEVICE_ADDR);
+}
+
+void usbh_set_address_cb(uint16_t addr, void* context)
+{
+    ESP_LOGI("ADDRESS", "%d", addr);
+    if(ESP_OK != hcd_pipe_update(ctrl_pipe_hdl, DEVICE_ADDR, bMaxPacketSize0)) ESP_LOGE("", "failed to update ctrl pipe");
+    xfer_set_configuration(1);
+}
+
+void usbh_get_config_desc_cb(uint8_t* data_buffer, size_t num_bytes, void* context)
+{
+    ESP_LOG_BUFFER_HEX_LEVEL("CONFIG descriptor", data_buffer, num_bytes, ESP_LOG_INFO);
+    parse_cfg_descriptor(data_buffer, 0, num_bytes, &conf_num);
+    xfer_get_string(1);
+    xfer_get_string(2);
+    xfer_get_string(3);    
+}
+
+void usbh_set_config_desc_cb(uint16_t data, void* context)
+{
+    ESP_LOGI("SET CONFIG", "%d", data);
+    xfer_get_desc();
+}
+
+void usbh_get_string_cb(uint8_t* data, size_t num_bytes, void* context)
+{
+    char out[64] = {};
+    utf16_to_utf8((char*)data, out, num_bytes);
+    ESP_LOGI("STRING CB", "[%d] %s", num_bytes, out);
+    parse_cfg_descriptor(data, 0, num_bytes, &conf_num);
+    xfer_get_current_config();
+}
+
+void usbh_ctrl_pipe_stalled_cb(usb_ctrl_req_t* ctrl)
+{
+    ESP_LOG_BUFFER_HEX_LEVEL("STALLED", ctrl, 8, ESP_LOG_WARN);
+}
+
+void usbh_ctrl_pipe_error_cb(usb_ctrl_req_t* ctrl)
+{
+    ESP_LOG_BUFFER_HEX_LEVEL("ERROR", ctrl, 8, ESP_LOG_WARN);
+}
+
+void usbh_get_configuration_cb(uint8_t addr, void* context)
+{
+    ESP_LOGI("GET CONFIG", "%d", addr);
+}
+
+#else
+// alternative way to handle ctrl pipe events
+static void ctrl_pipe_cb(pipe_event_msg_t msg, usb_irp_t *irp, void *context)
+{
+    // hcd_pipe_handle_t pipe_hdl;
+    // hcd_xfer_req_get_target(req_hdl, &pipe_hdl, &irp, &context);
+    ESP_LOGW("", "\t-> Pipe [%d] event: %d\n", (uint8_t)context, msg.pipe_event);
+
+    usb_ctrl_req_t* ctrl = (usb_ctrl_req_t*)irp->data_buffer;
 
     switch (msg.pipe_event)
     {
@@ -48,7 +116,6 @@ static void ctrl_pipe_cb(pipe_event_msg_t msg, hcd_xfer_req_handle_t req_hdl)
         case HCD_PIPE_EVENT_XFER_REQ_DONE:
             ESP_LOGD("Pipe: ", "XFER status: %d, num bytes: %d, actual bytes: %d", irp->status, irp->num_bytes, irp->actual_num_bytes);
             if(msg.pipe_hdl == ctrl_pipe_hdl){
-                usb_ctrl_req_t* ctrl = (usb_ctrl_req_t*)irp->data_buffer;
 
                 ESP_LOG_BUFFER_HEX_LEVEL("Ctrl data", ctrl, sizeof(usb_ctrl_req_t), ESP_LOG_DEBUG);
                 ESP_LOG_BUFFER_HEX_LEVEL("Actual data", irp->data_buffer + sizeof(usb_ctrl_req_t), irp->actual_num_bytes, ESP_LOG_DEBUG);
@@ -81,6 +148,7 @@ static void ctrl_pipe_cb(pipe_event_msg_t msg, hcd_xfer_req_handle_t req_hdl)
         
         case HCD_PIPE_EVENT_ERROR_STALL:
             ESP_LOGW("", "Device stalled: %s pipe, state: %d", msg.pipe_hdl == ctrl_pipe_hdl?"CTRL":"BULK", hcd_pipe_get_state(msg.pipe_hdl));
+                ESP_LOG_BUFFER_HEX_LEVEL("Ctrl data", ctrl, sizeof(usb_ctrl_req_t), ESP_LOG_INFO);
             hcd_pipe_command(msg.pipe_hdl, HCD_PIPE_CMD_RESET);
             break;
         
@@ -90,7 +158,50 @@ static void ctrl_pipe_cb(pipe_event_msg_t msg, hcd_xfer_req_handle_t req_hdl)
     }
 }
 
-// example of port callback
+#endif
+/*------------------------------- USBH EP0 CTRL PIPE CALLBACKS -------------------------------*/
+
+/*------------------------------- USBH PORT CALLBACKS -------------------------------*/
+#ifndef USE_ALTERNATIVE_CALLBACKS
+void usbh_port_connection_cb(port_event_msg_t msg)
+{
+    hcd_port_state_t state;
+    ESP_LOGI("", "HCD_PORT_EVENT_CONNECTION");
+    if(HCD_PORT_STATE_DISABLED == hcd_port_get_state(msg.port_hdl)) ESP_LOGI("", "HCD_PORT_STATE_DISABLED");
+    if(ESP_OK == hcd_port_command(msg.port_hdl, HCD_PORT_CMD_RESET)) ESP_LOGI("", "USB device reset");
+    if(HCD_PORT_STATE_ENABLED == hcd_port_get_state(msg.port_hdl)){
+        ESP_LOGI("", "HCD_PORT_STATE_ENABLED");
+        // we are already physically connected and ready, now we can perform software connection steps
+        alloc_pipe_and_xfer_reqs_ctrl(msg.port_hdl, &ctrl_pipe_hdl);
+        // get device descriptor on EP0, this is first mandatory step
+        xfer_get_device_desc();
+    }
+}
+
+// void usbh_port_disconnection_cb(port_event_msg_t){}
+// void usbh_port_error_cb(port_event_msg_t){}
+// void usbh_port_overcurrent_cb(port_event_msg_t){}
+
+void usbh_port_sudden_disconn_cb(port_event_msg_t msg)
+{
+    hcd_port_state_t state;
+    if (HCD_PIPE_STATE_INVALID == hcd_pipe_get_state(ctrl_pipe_hdl))
+    {                
+        ESP_LOGW("", "pipe state: %d", hcd_pipe_get_state(ctrl_pipe_hdl));
+        free_pipe_and_xfer_reqs_ctrl(ctrl_pipe_hdl);
+        ctrl_pipe_hdl = NULL;
+
+        esp_err_t err;
+        if(HCD_PORT_STATE_RECOVERY == (state = hcd_port_get_state(msg.port_hdl))){
+            if(ESP_OK != (err = hcd_port_recover(msg.port_hdl))) ESP_LOGE("recovery", "should be not powered state %d => (%d)", state, err);
+        } else {
+            ESP_LOGE("", "hcd_port_state_t: %d", state);
+        }
+        if(ESP_OK == hcd_port_command(msg.port_hdl, HCD_PORT_CMD_POWER_ON)) ESP_LOGI("", "Port powered ON");
+    }
+}
+#else
+// alternative way to handle port events
 static void port_cb(port_event_msg_t msg)
 {
     hcd_port_state_t state;
@@ -141,27 +252,23 @@ static void port_cb(port_event_msg_t msg)
         default:
             ESP_LOGE("", "port event: %d", msg.port_event);
             break;
-    }    
+    }
 }
+#endif
+/*------------------------------- USBH PORT CALLBACKS -------------------------------*/
 
 void app_main(void)
 {
     printf("Hello world USB host!\n");
     if(setup_usb_host()){
         xTaskCreate(ctrl_pipe_event_task, "pipe_task", 4*1024, NULL, 10, NULL);
-
+#ifdef USE_ALTERNATIVE_CALLBACKS
         register_port_callback(port_cb);
         register_ctrl_pipe_callback(ctrl_pipe_cb);
+#endif
     }
 
     while(1){
         vTaskDelay(1000);
-        if(HCD_PORT_STATE_DISCONNECTED != hcd_port_get_state(port_hdl)){
-            xfer_get_string(1);
-            vTaskDelay(1);
-            xfer_get_string(2);
-            vTaskDelay(1);
-            xfer_get_string(3);
-        }
     }
 }
